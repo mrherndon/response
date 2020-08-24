238d6a1656fc05c2dd74e651cd77c1bc2fd76676
Raymond Herndon <rherndon@starsacramento.org>
2020-08-24 14:21:10 -0500

    first commit

---
+++ src/response.php
@@ -0,0 +1,23 @@
+<?php
+namespace star\response;
+
+class response {
+	public $message = 'Post Successful';
+	public $object;
+	public $error = false;
+	public $errorMessage = '';
+
+	public function __construct(object $object){
+        $this->object = !empty($object) ? $object : new \stdClass();
+	}
+	
+	public function return(string $message = '', string $error = '') {
+		if(!empty($error)) {
+			$this->error = true;
+			$this->errorMessage = $error;
+		}
+		if(!empty($message))$this->message = $message;
+		echo(json_encode($this));
+		exit;
+	}
+}
